ce4b917c8e6930607fc808c6fb467e01b8fea5b6
catirito <catirito30@gmail.com>
2015-05-16 15:05:17 +0200

    First commit

---
+++ AlumnoTest.java
@@ -0,0 +1,79 @@
+package unit9_b;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlumnoTest {
+
+	@Test
+	public final void testValidaNif() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCalculaTasaMatricula() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
